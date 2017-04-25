8121183fd4bae976ce1a0d5ab9f5a8b7b52e0c8e
Li ZhenHui <phetsi_w@etna-alternance.net>
2017-04-25 22:58:44 +0200

    Update type.c

--- Sources/helpers/type.c
+++ Sources/helpers/type.c
@@ -2,11 +2,11 @@
 #include"../../Headers/helpers/print.h"
 #include"../../Headers/helpers/type.h"
 
-short int       is_digit(char * s)
+_Bool       is_digit(char * s)
 {
-    int         r;
-    short int   b;
-    regex_t     x;
+    int     r;
+    _Bool   b;
+    regex_t x;
 
     if ((r = regcomp(&x, "^[:digit:]", 0))) return 0;
     if (!(r = regexec(&x, s, 0, 0, 0)))  b = 0;
@@ -16,11 +16,11 @@ short int       is_digit(char * s)
     return b;
 }
 
-short int       is_alnum(char * s)
+_Bool       is_alnum(char * s)
 {
-    int         r;
-    short int   b;
-    regex_t     x;
+    int     r;
+    _Bool   b;
+    regex_t x;
 
     if ((r = regcomp(&x, "^[:alnum:]", 0))) return 0;
     if (!(r = regexec(&x, s, 0, 0, 0))) b = 0;
