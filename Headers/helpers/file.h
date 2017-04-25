4fb8145b647e6772d065b4c4ee41049dd3d315d8
Li ZhenHui <phetsi_w@etna-alternance.net>
2017-04-25 22:59:55 +0200

    Update file.h

--- Headers/helpers/file.h
+++ Headers/helpers/file.h
@@ -8,7 +8,7 @@
 
 /* Usage functions */
 
-int     read_file   (game_t * g, char * d);
+_Bool   read_file   (game_t * g, char * d);
 size_t  file_size   (const char * d);
 
 #endif //__SNAKE_FILE_H__
