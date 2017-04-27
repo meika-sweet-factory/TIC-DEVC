e9832dd659310393d9e380b6ace4c27fe92a67a1
asuramaru <asuramaru@wonderland>
2017-04-27 11:30:08 +0200

    fix norme again

--- Headers/map.h
+++ Headers/map.h
@@ -1,8 +1,8 @@
 #ifndef _SNAKE_MAP_H_
 #define _SNAKE_MAP_H_
 
-#include"structures/type.h"
-#include"structures/game.h"
+#include "structures/type.h"
+#include "structures/game.h"
 
 _Bool generate_map(t_game * restrict g, const char * restrict x, const char * restrict y);
 
