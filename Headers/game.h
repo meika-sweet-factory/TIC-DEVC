73400e6660a2e763bacdba0cc8c0b63df676f73d
asuramaru <asuramaru@wonderland>
2017-04-27 11:08:43 +0200

    boolean land

--- Headers/game.h
+++ Headers/game.h
@@ -8,8 +8,8 @@
 
 /* Usage functions */
 
-int     new_game    (t_game * g, const char * x, const char * y, _Bool (* callback)(t_game * g));
-int     load_game   (t_game * g, const char * f, _Bool (* callback)(t_game * g));
+_Bool   new_game    (t_game * g, const char * x, const char * y, _Bool (* callback)(t_game * g));
+_Bool   load_game   (t_game * g, const char * f, _Bool (* callback)(t_game * g));
 _Bool   play_game   (t_game * g);
 
 #endif //_SNAKE_GAME_H_
