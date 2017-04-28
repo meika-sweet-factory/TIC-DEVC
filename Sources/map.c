9cb807bcfc443d5af069176489d5d0aca8dc91f1
asuramaru <asuramaru@wonderland>
2017-04-28 10:28:23 +0200

    fixing map reduced

--- Sources/map.c
+++ Sources/map.c
@@ -4,25 +4,25 @@
 #include "../Headers/player.h"
 #include "../Headers/map.h"
 
-_Bool       generate_map(t_game * g, t_axe a)
+_Bool       generate_map(t_game * g, t_axe s)
 {
-    g->map.size.x = a.x;
-    g->map.size.y = a.y;
-    if (!(g->map.board = init_map(a.x, a.y))) return ERROR;
-    for (a.y = 0; a.y < a.y; ++a.y) {
-        if (!(g->map.board[a.y] = init_map_cell(a.x))) return ERROR;
-        for (a.x = 0; a.x < a.x; ++a.x)
-            if ((a.x == 0)||(a.x == a.x - 1))
-                g->map.board[a.y][a.x] = '1';
-            else if (((a.y == 0) || (a.y == a.y -1)) && a.x != a.x -1) 
-                g->map.board[a.y][a.x] = '1';
-            else if (a.x != a.x)
-                g->map.board[a.y][a.x] = ' ';
+    t_axe   a;
+
+    g->map.size.x = s.x;
+    g->map.size.y = s.y;
+    if (!(g->map.board = init_map(s.x, s.y))) return ERROR;
+    for (a.y = 0; a.y < s.y; ++a.y) {
+        if (!(g->map.board[i] = init_map_cell(s.x))) return ERROR;
+        for (a.x = 0; a.x < s.x; ++a.x) {
+            if ((j == 0)||(j == s.x - 1)) g->map.board[a.y][a.x] = '1';
+            else if (((i == 0) || (i == s.y -1)) && j != s.x -1) g->map.board[a.y][a.x] = '1';
+            else if (a.x != s.x) g->map.board[a.y][a.x] = ' ';
+        }
         g->map.board[a.y][a.x] = '\0';
-        a.x = 0;
+        j = 0;
     }
     generate_spawn(g);
-    if (!generate_snake(g)) return ERROR;
+    generate_snake(g);
     return SUCCESS;
 }
 
