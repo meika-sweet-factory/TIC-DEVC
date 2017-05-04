bffca5db9007dfc2d2c8183251766a8a1982e2d0
asuramaru <asuramaru@wonderland>
2017-05-04 11:11:37 +0200

    rename

--- Headers/common.h
+++ Headers/common.h
@@ -1,7 +1,6 @@
-#ifndef _SNAKE_STUCTURE_TYPE_H_
-#define _SNAKE_STUCTURE_TYPE_H_
+#ifndef _SNAKE_COMMON_H_
+#define _SNAKE_COMMON_H_
 
-#define VOID
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
 #define ERROR 0
@@ -21,4 +20,4 @@ static const unsigned WINDOW_HEIGHT = 600;
 static const unsigned BOX_WIDTH = 256;
 static const unsigned BOX_HEIGHT = 256;
 
-#endif //_SNAKE_STUCTURE_TYPE_H_
+#endif //_SNAKE_COMMON_H_
